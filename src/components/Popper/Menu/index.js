fc084008e18ebf13a7d4316b37336fff9dc779f2
Minhthien1122 <thiennm1112@gmail.com>
2022-12-11 15:00:02 +0700

    Sửa lỗi và hoàn thiện phần Header #2

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -52,7 +52,7 @@ function Menu({ children, items = [], hideOnClick = false, onchange = defaultFn 
                                 }}
                             />
                         )}
-                        {renderItems()}
+                        <div className={cx('menu-body')}>{renderItems()}</div>
                     </PopperWrapper>
                 </div>
             )}
