c69ec1203b9b96bddf251ffcffceb7b29e603481
Minhthien1122 <thiennm1112@gmail.com>
2022-12-12 14:11:55 +0700

    Xây dựng UI phần sidebar #3

---
+++ src/components/SuggestedAccounts/AccountPreview/AccountPreview.js
@@ -0,0 +1,39 @@
+import classNames from 'classnames/bind';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import Button from '~/components/Button';
+import styles from './AccountPreview.module.scss';
+import Image from '~/components/Images/Images';
+
+const cx = classNames.bind(styles);
+
+function AccountPreview() {
+    return (
+        <div className={cx('wrapper')}>
+            <header className={cx('header')}>
+                <Image
+                    className={cx('avatar')}
+                    src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/038bb1fc805bc8cfc190cc561f3f7a60~c5_100x100.jpeg?x-expires=1670997600&x-signature=CjuO5jUp%2BR5rZKI5rM13Ea5acIY%3D"
+                    alt=""
+                />
+                <Button className={cx('follow-btn')} primary>Follow</Button>
+            </header>
+            <div className={cx('body')}>
+                <p className={cx('nickname')}>
+                    <strong>quoc</strong>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <p className={cx('name')}>đwd</p>
+                <p className={cx('analytics')}>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Followers</span>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Likes</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AccountPreview;
--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -1,9 +1,17 @@
 import classNames from 'classnames/bind';
 
-import styles from './Sidebar.module.scss';
-import { HomeIcon, HomeActiveIcon, UserGroupIcon, UserGroupActiveIcon, LiveIcon, LiveActiveIcon } from '~/components/Icons';
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    UserGroupIcon,
+    UserGroupActiveIcon,
+    LiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
 import Menu, { MenuItem } from './Menu';
 import config from '~/config';
+import SuggestedAccounts from '~/components/SuggestedAccounts';
+import styles from './Sidebar.module.scss';
 
 const cx = classNames.bind(styles);
 
@@ -11,10 +19,23 @@ function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
             <Menu>
-                <MenuItem title={'For you'} to={config.routes.home} icon={<HomeIcon />} activeIcon = {<HomeActiveIcon/>}/>
-                <MenuItem title={'Following'} to={config.routes.following} icon={<UserGroupIcon />} activeIcon = {<UserGroupActiveIcon/>}/>
-                <MenuItem title={'LIVE'} to={config.routes.live} icon={<LiveIcon />} activeIcon = {<LiveActiveIcon/>}/>
+                <MenuItem
+                    title={'For you'}
+                    to={config.routes.home}
+                    icon={<HomeIcon />}
+                    activeIcon={<HomeActiveIcon />}
+                />
+                <MenuItem
+                    title={'Following'}
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title={'LIVE'} to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
             </Menu>
+
+            <SuggestedAccounts label="Suggested accounts" />
+            <SuggestedAccounts label="Following accounts" />
         </aside>
     );
 }
